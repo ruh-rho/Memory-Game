98a76fe0140527b6b3fbf25ab8770774e403405e
kev <oomph.burn@gmail.com>
2018-08-02 17:58:49 -0400

    added css styling to cards

--- Website/js/app.js
+++ Website/js/app.js
@@ -7,10 +7,20 @@ function generateCards()
     let canvas = document.getElementById("canvas");
     for(let cardNumber = 0; cardNumber < HEIGHT*WIDTH; cardNumber++)
     {
-        let newCard = document.createElement("div");
-        let textNode = document.createTextNode(cardNumber);
-        newCard.appendChild(textNode);
-        newCard.className = "card";
-        canvas.appendChild(newCard);
+        let cardDiv = document.createElement("div");
+        let cardNumberText = document.createTextNode(cardNumber + 1);
+
+        cardDiv.appendChild(cardNumberText);
+        cardDiv.className = "card";
+
+        let cardContentDiv = document.createElement("div");
+        let cardContentText = document.createTextNode("A");
+        cardContentDiv.appendChild(cardContentText);
+        cardContentDiv.className = "card__content";
+        
+
+        cardDiv.appendChild(cardContentDiv);
+
+        canvas.appendChild(cardDiv);
     }
 }
